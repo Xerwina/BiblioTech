3194cf48e603c29114045b9fb142129d9708e1c0
Xerwina <werlino@gmail.com>
2026-01-08 09:15:35 +0100

    Add files via upload

---
+++ BiblioTech/Controllers/Auteur.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioTech.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BiblioTech.Controllers
+{
+
+    internal class Auteur
+
+    {
+        private static BiblioTechEntities1 db = new BiblioTechEntities1();
+
+        public  static bool AjoutAuteur( String nom, String bio)
+        {
+            Models.Auteur a1 = new Models.Auteur() { nom = nom, bio = bio};
+            db.Auteur.Add(a1);
+            return db.SaveChanges() == 0;
+        }
+
+        public Boolean RecupAuteur(String nom)
+        {
+            string connectionBd =
+     @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BiblioTech;Integrated Security=True;";
+            string queryGetAuteur = @"SELECT (nom) FROM Auteur";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionBd))
+                {
+                    using (SqlCommand cmd = new SqlCommand(queryGetAuteur, conn))
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public static Models.Auteur RecupAuteurProf(string nom)
+        {
+            return db.Auteur.SingleOrDefault(x => x.nom == nom);
+        }
+    }
+}
+
---
+++ BiblioTech/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioTech.Controllers;
+using BiblioTech.Models;
+
+namespace BiblioTech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // String nom;
+            // String bio;
+
+            // Console.WriteLine("rentre un ptn d'auteur");
+            // nom = Console.ReadLine();
+
+            // Console.WriteLine("rentre une bio");
+            // bio = Console.ReadLine();
+
+            //Controllers.Auteur a1 = new Controllers.Auteur();
+            //a1.AjoutAuteur(nom, bio);
+
+            if(Controllers.Auteur.AjoutAuteur("titi", "tutu"))
+                Console.WriteLine("Nouvel Auteur");
+            else
+                Console.WriteLine("Nop ! Pas envie");
+
+
+            Console.WriteLine("Recherche un auteur");
+            Console.Write("Nom de l'auteur : ");
+            string auteurName = Console.ReadLine();
+            Models.Auteur aut = Controllers.Auteur.RecupAuteurProf(auteurName);
+            if (aut != null)
+                Console.WriteLine("Auteur trouvé");
+            else
+                Console.WriteLine("Nop");
+            //Controllers.Auteur getAuteur = new Controllers.Auteur();
+            //getAuteur = Console.ReadLine();
+            //Console.WriteLine(getAuteur.RecupAuteur());
+
+
+
+
+
+        }
+    }
+}
